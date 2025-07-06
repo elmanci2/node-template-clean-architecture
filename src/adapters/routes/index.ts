a64896af4a36002d746806138ffcfd24e3b80a03
Andres Felipe Mancilla <85756914+elmanci2@users.noreply.github.com>
2025-07-06 14:29:24 -0500

    Update index.ts

--- src/adapters/routes/index.ts
+++ src/adapters/routes/index.ts
@@ -1,9 +1,21 @@
-import authRoutes from './list/auth/auth';
-import { RoutesManager } from './manage/RouteManager';
+
+import authRoutes from "./list/auth/auth";
+import { RoutesManager } from "./manage/RouteManager";
 
 const routesManager = new RoutesManager();
 
-routesManager.addRoutes(authRoutes)
-    .addGlobalMiddlewares([]);
 
-export default routesManager as RoutesManager;
+
+routesManager.globalMiddlewares({
+    keys: ["Usuarios", "Vehículos", "Chat_AI", "Uploads"],
+    middlewares: [],
+})
+
+routesManager.addRoutes({
+    name: "Auth",
+    description: "Rutas de autenticación (login, registro)",
+    prefix: "/auth",
+    routes: authRoutes,
+});
+
+export default routesManager;
